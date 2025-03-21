98c758a334e0eb561814ab84af56a234be3febe6
JK <397359+bluesman80@users.noreply.github.com>
2025-03-21 15:21:44 +0100

    Initial commit

---
+++ src/main/java/nl/jimkaplan/autotrader/AutotraderApplication.java
@@ -0,0 +1,13 @@
+package nl.jimkaplan.autotrader;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AutotraderApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AutotraderApplication.class, args);
+	}
+
+}
