bee535b4ea574463b69e48bdbf3b19d94615afbe
JK <397359+bluesman80@users.noreply.github.com>
2025-03-28 22:45:42 +0100

    ♻️ refactor(BitvavoConfig): Remove redundant @ConfigurationProperties annotation

--- src/main/java/nl/jimkaplan/autotrader/config/BitvavoConfig.java
+++ src/main/java/nl/jimkaplan/autotrader/config/BitvavoConfig.java
@@ -2,12 +2,10 @@ package nl.jimkaplan.autotrader.config;
 
 import lombok.Data;
 import org.springframework.beans.factory.annotation.Value;
-import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.context.annotation.Configuration;
 
 @Data
 @Configuration
-@ConfigurationProperties(prefix = "bitvavo")
 public class BitvavoConfig {
     @Value("${bitvavo.apiKey}")
     private String apiKey;
